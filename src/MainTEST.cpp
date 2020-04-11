0f4dde6fdf22b5b13a50ae4d42fcb32ff5e37afc
Lapo9 <lapofalcone@gmail.com>
2020-04-12 00:29:07 +0200

    Added experimental features for multiple objects
    Added the overload of ',' operator to create a list of ThreadSafe objects and lock them for the duration of the full expression where the list is present. Added a new type (Locks) to hold the list of locked mutexes. Added the declaration of '->*' operator to call a function after the lockdown of one or more ThreadSafe objects. Debug of each of these new features is strongly needed

--- src/MainTEST.cpp
+++ src/MainTEST.cpp
@@ -1,9 +1,37 @@
 #include <iostream>
 #include <string>
+#include <functional>
 #include "ThreadSafe.h"
 
+
+void scratch() {
+
+    thread_safe::ThreadSafe<std::string> safe2{"safe2"};
+    thread_safe::ThreadSafe<std::string> safe3{"safe3"};
+    thread_safe::ThreadSafe<std::string> safe4{"safe4"};
+    thread_safe::ThreadSafe<std::string> safe5{"safe5"};
+
+    //safe2->*appendAndLenght(~safe2, 14.9);
+    //safe2->*std::function([](int x, float y)->void {});
+    //safe2->*std::function(appendAndLenght);
+
+    (safe2, safe3, safe4, safe5).printTEST();
+
+    safe2->append(" free");
+    std::cout << std::string(*safe2);
+
+}
+
+int appendAndLenght(std::string app, float x) {
+    app.append(std::to_string(x));
+    int y = app.size();
+    return y;
+}
+
+
+
 int main() {
-    thread_safe::ThreadSafe<std::string> safe1("Ciao");
+    thread_safe::ThreadSafe<std::string> safe1{"Ciao"};
     thread_safe::ThreadSafe<std::string>* pSafe1 = &thread_safe::ThreadSafe<std::string>("Pointer");
 
     safe1->append("oooooooooo"); //append something to the protected string
@@ -20,5 +48,9 @@ int main() {
     std::cout << "\n\n" << std::string(**pSafe1); //print the string safely
     std::cout << "\n\n" << ~*pSafe1; //print the string NOT safely
 
-    int x; std::cin>>x;
+    std::cout << "\n\n\n\n\n=========================================================================================================\n\n\n\n\n";
+
+    scratch();
+
+    int x; std::cin >> x;
 }
--- src/ThreadSafe.h
+++ src/ThreadSafe.h
@@ -2,9 +2,13 @@
 #define THREAD_SAFE_OBJECT
 
 #include <mutex>
+#include <functional>
+#include <vector>
 #include <iostream> //DEBUG
 
 namespace thread_safe {
+
+	struct Locks;
 
 	//a class that stores an object of any type and a mutex. Each time the object is accessed, the mutex is locked, and unlocked at the end of the access.
 	template <typename WrappedType>
@@ -15,18 +19,20 @@ namespace thread_safe {
 			ThreadSafe& real; //the reference to the permanent object
 			std::lock_guard<std::mutex> guard {real.mtx};
 
+
 			public:
-			Temp(ThreadSafe& real) : real {real} {}
+			Temp(ThreadSafe& real) : real{real} {}
+
 
 			//returns the object wrapped in ThreadSafe (pointer because -> needs a pointer as return type)
 			WrappedType* operator->() {
-				std::cout << "Temp ->\n"; //DEBUG
+				std::cout << "\x1B[31mTemp ->\033[0m\n"; //DEBUG
 				return &real.wrappedObj;
 			}
 
 			//convert the temp type to the object it holds
 			operator WrappedType&() {
-				std::cout << "Temp impl cast\n"; //DEBUG
+				std::cout << "\x1B[31mTemp ->\033[0m\n"; //DEBUG
 				return real.wrappedObj;
 			}
 
@@ -41,16 +47,28 @@ namespace thread_safe {
 		//constructs the wrapped object of type WrappedType using perfect forwarding
 		template<typename ...ArgsType>
 		ThreadSafe(ArgsType&&... args) : wrappedObj(std::forward<ArgsType>(args)...) {}
+		
+		ThreadSafe(ThreadSafe& ts) : wrappedObj{ts.wrappedObj} {}
+		ThreadSafe& operator=(ThreadSafe& ts) {
+			wrappedObj = ts.wrappedObj;
+			return *this;
+		}
+		ThreadSafe(ThreadSafe&& ts) : wrappedObj{ std::move(ts.wrappedObj)} {}
+		ThreadSafe& operator=(ThreadSafe&& ts) {
+			wrappedObj = std::move(ts.wrappedObj);
+		}
+		
+
 
 		//returns a temporary object which holds a reference to this object and locks the mutex on its creation. The temporary object has a -> which returns a reference to the wrapped object
 		Temp operator->() {
-			std::cout << "ThreadSafe ->\n"; //DEBUG
+			std::cout << "\x1B[31mThreadSafe ->\033[0m\n"; //DEBUG
 			return Temp(*this);
 		}
 
 		//returns a temporary object which holds a reference to this object and locks the mutex on its creation. The temporary object has a cast operator which returns a reference to the wrapped object
 		Temp operator*() {
-			std::cout << "ThreadSafe *\n"; //DEBUG
+			std::cout << "\x1B[31mThreadSafe *\033[0m\n"; //DEBUG
 			return Temp(*this);
 		}
 
@@ -61,17 +79,78 @@ namespace thread_safe {
 
 		//returns the wrapped object without locking the mutex
 		WrappedType& operator~() {
-			std::cout << "ThreadSafe ~\n"; //DEBUG
+			std::cout << "\x1B[31mThreadSafe ~\033[0m\n"; //DEBUG
 			return wrappedObj;
 		}
 
 		WrappedType& operator--(int) {
-			std::cout << "ThreadSafe --\n"; //DEBUG
+			std::cout << "\x1B[31mThreadSafe --\033[0m\n"; //DEBUG
 			return wrappedObj;
+		}
+
+
+
+		//the function to be called after the lockdown of the mutex
+		template<typename Return, typename ...Args>
+		void operator->*(std::function<Return(Args...)> callable) {
+			std::lock_guard<std::mutex>(mtx);
+			//TODO everything
+		}		
+
+
+		//if two ThreadSafe objects are separated by a comma, locks those objects' mutexes and return the object representing the list of the locked mutexes
+		template <typename A, typename B>
+		friend Locks operator,(ThreadSafe<A>& ts1, ThreadSafe<B>& ts2) {
+			return Locks(std::unique_lock<std::mutex>(ts1.mtx), std::unique_lock<std::mutex>(ts2.mtx));
+		}
+
+		/*if a comma-separated list of locked mutexes and a ThreadSafe object is present, locks the new object's mutex and add it to the already existent list.
+		Actually a new list is created, so that it is a temporary list, so it is destroyed as soon as the full expression containing the list ends.
+		The order of the argument (Locks, ThreadSafe<A>) is relevant, since the comma operator as a left-to-right associativity. This way it is possible to write expressions of the kind `SomeType obj = (ts1, ts2, ts3, ..., tsN)->*foobar(...);`. (Note that the use of parentesis to enclose the comma separated list is necessary, sinche comma is the least prioritized operator in C++.*/
+		//TODO maybe it is possible just to add the new locked mutex to the Locks list (instead of creating a new list)?
+		template <typename A>
+		friend Locks operator,(Locks locks, ThreadSafe<A>& ts) {
+			return Locks(std::move(locks), std::unique_lock<std::mutex>(ts.mtx));
 		}
 
 	};
 
+
+	//this object contains an array of unique_locks. It is instantiated temporarily, so that all of the ThreadSafe objects in a comma separated list are locked for the duration of the statement.
+	//TODO there is the big problem that this mechanism is not deadlock safe. Example: 2 different threads run at the same time (ts1, ts2, ts3)->*... and (ts3, ts2, ts1)->*... The first thread locks ts1, the second thread locks ts3, the first locks ts2, the second tries to lock ts2 (but cannot), the first triest to lock ts3 (but cannot). A way to solve this is to make a thread release all of the mutex acquired in the comma separated list as soon as it fails to lock a mutex (and retry immediately), but there is the risk that the same situation goes on forever! Maybe it is possible to implement an algorithm similiar to the CSMACA (for wifi) but it would be slow (I think).
+	struct Locks {
+		//TODO maybe make the vector private?
+		std::vector<std::unique_lock<std::mutex>> lockGuards;
+
+		//constructs a Locks of 2 unique_lock(s)
+		Locks(std::unique_lock<std::mutex>&& lg1, std::unique_lock<std::mutex>&& lg2) {
+			lockGuards.push_back(std::move(lg1));
+			lockGuards.push_back(std::move(lg2));
+		}
+
+		//constructs a Locks from another Locks and a unique_lock
+		Locks(Locks&& locks, std::unique_lock<std::mutex>&& lg) {
+			//TODO DEBUG uncomment next line and remove the for loop if you understand they are semantically equivalent
+			//lockGuards = std::move(locks.lockGuards);
+			for (auto& lock : locks.lockGuards) {
+				lockGuards.push_back(std::move(lock));
+			}
+
+			lockGuards.push_back(std::move(lg));
+		}
+
+		//the function to be called after the lockdown of the mutexes
+		template<typename Return, typename ...Args>
+		void operator->*(std::function<Return(Args...)> callable) {
+			//TODO everything
+		}
+
+		//DEBUG to remove
+		void printTEST() { 
+			std::cout << "\x1B[32mTEST\033[0m";
+			std::cout << "\x1B[32m, lock count: \033[0m " << lockGuards.size() << "\n"; 
+		}
+	};
 }
 
 #endif
